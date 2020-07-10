23b0b928768d6c747579693fe56e6143ebd52720
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2020-07-10 23:36:35 +0200

    Deleted unused parts

--- keep/{# client-sdk #}/codegen.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-# FIXME: activate when ITIFoundation/openapi-tools:latest available
-exec {{ cookiecutter.simcore_install_root }}../scripts/openapi/openapi_codegen.sh \
-    -i ../src/{{ cookiecutter.package_name }}/oas3/{{cookiecutter.openapi_specs_version}}/openapi.yaml \
-    -o . \
-    -g python \
-    -c ./codegen_config.json
--- keep/{# integration #}/conftest.py
+++
@@ -1,9 +0,0 @@
-
-
-# TODO: docker-compose -f services/docker-compose.yml up --exit-code-from={{ cookiecutter.project_name }}  {{ cookiecutter.project_name }}
-
-# TODO: docker-compose -f services/docker-compose.yml up --detach {{ cookiecutter.project_name }}
-# test if responsive ??
-
-# ls home
-# ls /devel
