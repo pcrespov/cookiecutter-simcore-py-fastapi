8f1eb8e873466ef13da3c83f014c71588e5ff5a0
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2020-07-10 23:36:47 +0200

    Minor

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,7 +1,8 @@
 import os
 from pathlib import Path
 
+
 work_dir = Path(os.getcwd()).resolve()
 module_name = "{{ cookiecutter.project_slug }}"
 
-print(f"Buidling {module_name} in '{work_dir}' ...")
+print(f"[PRE] Buidling {module_name} in '{work_dir}' ...")
