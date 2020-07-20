c0d45865f0da1ff91067ee83eb82681368dc01de
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-07-20 19:02:27 +0200

    simplified versioning

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/__version__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/__version__.py
@@ -3,9 +3,8 @@
 import pkg_resources
 
 __version__ = pkg_resources.get_distribution('{{ cookiecutter.package_name }}').version
-assert __version__=="{{ cookiecutter.version }}", "Did you `pip install` this package?" # nosec
 
 major, minor, patch = __version__.split(".")
 
-api_version = __version__
+api_version: str = __version__
 api_vtag: str = f"v{major}"
