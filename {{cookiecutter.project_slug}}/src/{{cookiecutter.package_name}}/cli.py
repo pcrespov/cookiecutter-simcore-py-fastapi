202545bb6b1ecd28cf9ca623aadfe58d73acec80
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-09-09 18:47:16 +0200

    Extra CLI on the app

---
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/cli.py
@@ -0,0 +1,23 @@
+import logging
+
+import typer
+from settings_library.utils_cli import create_settings_command
+
+from ._meta import PROJECT_NAME
+from .core.settings import ApplicationSettings
+
+log = logging.getLogger(__name__)
+main = typer.Typer(name=PROJECT_NAME)
+
+
+main.command()(create_settings_command(settings_cls=ApplicationSettings, logger=log))
+
+
+@main.command()
+def run():
+    """Runs application"""
+    typer.secho("Sorry, this entrypoint is intentionally disabled. Use instead")
+    typer.secho(
+        "$ uvicorn {{cookiecutter.package_name}}.main:the_app",
+        fg=typer.colors.BLUE,
+    )
