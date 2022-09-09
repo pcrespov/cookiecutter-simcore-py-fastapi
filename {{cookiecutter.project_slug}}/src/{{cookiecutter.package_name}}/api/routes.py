8ef3c99faef7e2fd67669fda196c30d006f9fb6e
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-09-09 17:49:51 +0200

    simplifies api skeleton

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/api/routes.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/api/routes.py
@@ -3,9 +3,11 @@
 """
 from fastapi import APIRouter, FastAPI
 from .._meta import api_vtag
+from . import _healthcheck
 
 
 def setup_api(app: FastAPI):
     router = APIRouter()
 
+    # FIXME: _healthcheck
     app.include_router(router, prefix=f"/{api_vtag}")
